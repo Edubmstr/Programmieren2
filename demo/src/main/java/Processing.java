c9a196c22fe52d32f0de099eee54fa896b4148bd
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-05-27 13:28:11 +0200

    ja

---
+++ demo/src/main/java/Processing.java
@@ -0,0 +1,28 @@
+import processing.core.PApplet;
+import processing.core.*;
+
+public class Processing extends PApplet {
+
+
+        public static void main (String [] args ) {
+            PApplet.main("Processing");
+        }
+
+        public void settings () { // startup settings
+            size (800 , 600) ;
+        }
+
+        public void setup () { // setup called after settings
+            stroke (255 , 0 , 0 , 255) ;
+            textSize (72) ;
+        }
+
+        int bgCol = 0;
+        public void draw () { // draw called every frame
+            background ( bgCol ); // clear background
+            textSize ( bgCol ) ;
+            text (" Hello World !" ,10 ,300) ; // set text
+            bgCol = ( bgCol + 1) % 128; // animate color
+        }
+    }
+
---
+++ demo/src/main/java/TestProcessing.java
@@ -0,0 +1,7 @@
+import processing.core.PApplet;
+
+public class TestProcessing extends PApplet {
+    public static void main(String[] args){
+        PApplet.main(TestProcessing.class);
+    }
+}
